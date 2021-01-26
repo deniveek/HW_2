95d516530dd2b3b37d0f862f45e62829613d4dc4
denmilonov <denmilonov@gmail.com>
2021-01-26 11:24:43 +0300

    tst

--- HW2_1.py
+++
@@ -1,3 +0,0 @@
-tst_list = [11, 11.0, "11", (1, 1)]
-for i in tst_list:
-    print(i, ",", type(i))
--- HW2_4.py
+++
@@ -1,5 +0,0 @@
-user_input = input("введите строку\n").split()
-i = 0
-for word in user_input:
-    i += 1
-    print(f"{i}. {word[:10]}")
